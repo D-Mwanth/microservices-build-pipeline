02e9ad0fdad9754f229c0d4a753b60246c7d6039
D-Mwanth <dmwanthi2@gmail.com>
2024-03-05 17:50:28 +0300

    modified shouldBildInitially func

--- vars/shouldBuildInitially.groovy
+++ vars/shouldBuildInitially.groovy
@@ -1,4 +1,4 @@
 // Function to determine if a service should be built initially
-def shouldBuildInitially(service, buildNumb, existingServices) {
+def call(service, buildNumb, existingServices) {
     return (buildNumb == 1) || !existingServices.contains(service)
 }
