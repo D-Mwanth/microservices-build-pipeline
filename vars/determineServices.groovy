29fad642d7b3ee2a859543b442e685f3da4677a2
Daniel Mwanthi <77895975+D-Mwanth@users.noreply.github.com>
2024-09-19 21:36:22 +0300

    Update determineServices.groovy

--- vars/determineServices.groovy
+++ vars/determineServices.groovy
@@ -5,6 +5,6 @@ def call(buildNumb) {
         return serviceList.split(' ')
     } else {
         // Get modified services only or new services (services that were not there in the previous commit).
-        return sh(script: 'git diff --name-only HEAD~1 | grep "^src" | grep -v "/.gitignore" | cut -d "/" -f 2 | sort | uniq | tr "\\n" " "', returnStdout: true).trim().split(' ')
+        return sh(script: 'git diff --name-status HEAD~1 | grep -v "^D" | grep "src" | grep -v "/.gitignore" | cut -d "/" -f 2 | sort | uniq | tr "\\n" " "', returnStdout: true).trim().split(' ')
     }
 }
