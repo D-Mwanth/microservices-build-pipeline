4fbba962b22f6ed7e8924a4df0cbf272b741f1d0
Daniel Mwanthi <77895975+D-Mwanth@users.noreply.github.com>
2024-10-30 00:56:34 +0300

    Removed redundant comments from the `buildOrReBuildImage` function.

--- vars/buildOrReBuildImage.groovy
+++ vars/buildOrReBuildImage.groovy
@@ -1,4 +1,4 @@
-// Function to build or rebuild services
+// Function to build services
 def call(service, initialBuild) {
     script {
         echo "${initialBuild ? 'Building' : 'Rebuilding'} and Pushing ${service} image...."
@@ -8,12 +8,11 @@ def call(service, initialBuild) {
             // Set image name
             IMAGE_NAME = "${DOCKER_USER}/${service}"
 
-            // def currentVersion = sh(script: "grep \"image: ${IMAGE_NAME}:[^[:space:]]*\" ${KUBE_MANIFESTS_DIR}/${service}.yaml | awk -F ':' '{print \$3}'", returnStdout: true).trim()
-            // def currentVersion = sh(script: "grep 'image: ${IMAGE_NAME}:[^[:space:]]*' ${KUBE_MANIFESTS_DIR}/${service}.yaml | awk -F ':' '{print \$3}' | awk '{gsub(/^v/, ""); print}'", returnStdout: true).trim()
+           // Get the current version of the service image from its k8s manifest
             def currentVersion = sh(script: "grep \"image: ${IMAGE_NAME}:[^[:space:]]*\" ${KUBE_MANIFESTS_DIR}/${service}/${service}.yaml | awk -F ':' '{print \$3}' | awk '{gsub(/^v/, \"\"); print}'", returnStdout: true).trim()
 
             dir(serviceDir) {
-                // Build image
+                // Build image without using cache
                 sh "docker build --no-cache -t ${IMAGE_NAME} ."
 
                 // Tag and push image
@@ -24,12 +23,8 @@ def call(service, initialBuild) {
                 // Update manifest file
                 sh "sed -i '' -e 's|${IMAGE_NAME}:v${currentVersion}|${IMAGE_NAME}:v${initialBuild ? INITIAL_VERSION : incrementVersion(currentVersion)}|g' ${KUBE_MANIFESTS_DIR}/${service}/${service}.yaml || true"
 
-                // Remove the local images
+                // Remove all the local images
                 sh "docker images -q | xargs -r docker rmi -f || true"
-                // sh "docker rmi ${IMAGE_NAME}:latest"
-                // sh "docker rmi ${IMAGE_NAME}:v${initialBuild ? INITIAL_VERSION : incrementVersion(currentVersion)}"
-                // Remove dangling images
-                // sh 'docker image prune -f'
             }
         }
     }
